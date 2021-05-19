63cdbd03149618c575c9d95b7fb4165f53ab646e
Adrian Rodriguez <adrianuhu@gmail.com>
2021-05-19 17:35:20 +0200

    Created functions and exploits

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-05-2021 a las 14:53:09
+-- Tiempo de generación: 19-05-2021 a las 17:25:04
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.11
 
@@ -30,16 +30,28 @@ USE `hackme`;
 --
 
 CREATE TABLE `users` (
-  `name` varchar(255) NOT NULL
+  `name` varchar(10) NOT NULL,
+  `description` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`name`) VALUES
-('user1'),
-('user2');
+INSERT INTO `users` (`name`, `description`) VALUES
+('user1', 'ok'),
+('user2', '<script>console.log(\"dungeon master\")</script>'),
+('user3', '<script>alert(\"pwned\")</script>');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`name`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+
+$dbValue = '';
+$dbValueProtected = '';
+$script = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['user']) && $_POST['user'] != "") {
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_DATABASE', 'hackme');
+    $connection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+    mysqli_set_charset($connection, "utf8");
+    
+    $user = $_POST['user'];
+    $query=mysqli_query($connection, "SELECT name, description FROM users WHERE name like '$user'");
+    if (mysqli_num_rows($query) > 0 ) {
+        $ins = mysqli_fetch_all($query, MYSQLI_ASSOC)[0];
+        $dbValue = $ins['name'] . $ins['description'];
+        $dbValueProtected=mysqli_real_escape_string($connection, $dbValue);
+    }else {
+        $dbValue = "ERROR";
+        $dbValueProtected = "ERROR";
+    }
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['script']) && $_POST['script'] != "") {    
+    $script = "<script>" . $_POST['script'] . "</script>";
+    // Securice
+    // $script = htmlspecialchars($script);
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['scriptFunction']) && $_POST['scriptFunction'] != "") {    
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_DATABASE', 'hackme');
+    $connection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+    mysqli_set_charset($connection, "utf8");
+    
+    $scriptFunction = $_POST['scriptFunction'];
+    $scriptFunction=mysqli_real_escape_string($connection, $_POST['scriptFunction']);
+    $query=mysqli_query($connection, "update `users` set description = '$scriptFunction' where name like 'user2'");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <!-- Inyection:     ' or ''='        -->
+        <p>Regular Query: <span style="background-color:yellow"> user1 </span></p>
+        <p>SQL Inyection: <span style="background-color:yellow"> ' or ''=' </span></p>
+        <input type="text" name="user" id="user" placeholder="user">
+        <input type="submit" value="GO">
+        <p id="dbValue">Result without protection: <?=$dbValue?></p>
+        <p id="dbValue">Result with protection: <?=$dbValueProtected?></p>
+        <hr>
+        <!-- Cross Site Scripting -->
+        <p>Cross Site Scripting</p>
+        <input type="text" name="script" id="script" placeholder="alert('hacked')">
+        <input type="submit" value="GO">
+        <p id="dbValue">Script: <?=$script?></p>
+        <hr>
+        <!-- Cross Site Scripting Insert -->
+        <input type="text" name="scriptFunction" id="script" placeholder="<script>">
+        <input type="submit" value="GO">
+
+    </form>
+
+
+    
+</body>
+</html>
